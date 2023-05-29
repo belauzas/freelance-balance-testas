f6c1c95274dea94e940fefb6a97a1b50284115db
Monika <412.kri@gmail.com>
2023-05-29 09:28:16 +0300

    3 task

---
+++ js/main.js
@@ -0,0 +1,4 @@
+console.clear();
+
+const metai = ['sausis', 'vasaris', 'kovas', 'balandis', 'geguze', 'birzelis', 'liepa', 'rugpjutis', 'rugsejis', 'spalis', 'lapkritis', 'gruodis'];
+console.log(metai);
