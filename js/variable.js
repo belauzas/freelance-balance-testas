f5b7ad0b0204603b99449756a637bf8e7c3e534a
Kevinas <129725556+TadasRup@users.noreply.github.com>
2023-05-29 09:19:34 +0300

    variable

---
+++ js/variable.js
@@ -0,0 +1,14 @@
+const months = [
+  'Sausis',
+  'Vasaris',
+  'Kovas',
+  'Balandis',
+  'Geguze',
+  'Birzelis',
+  'Liepa',
+  'Rugjutis',
+  'Rugsejis',
+  'Spalis',
+  'Lapkritis',
+  'Gruodis'
+];
