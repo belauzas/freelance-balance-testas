b514c4475d3fb4bf241b38f86e09558805a92d0b
Kevinas <129725556+TadasRup@users.noreply.github.com>
2023-05-29 09:38:42 +0300

    months balance

--- js/variable.js
+++ js/variable.js
@@ -1,3 +1,5 @@
+//Menesiu pavadinimai
+
 const months = [
   'Sausis',
   'Vasaris',
@@ -12,3 +14,7 @@ const months = [
   'Lapkritis',
   'Gruodis'
 ];
+
+//Sugeneruoti "Metų balansa"
+
+var menesiuBalancas = [300, 500, 700, 900, 1100, 1300, 1500, 1700, 1900, 2100, 2300, 2500];
